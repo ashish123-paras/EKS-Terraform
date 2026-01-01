8e713428759d686da0f314e7f46206306a7abe2e
ashish123-paras <ashishchandila2100@gmail.com>
2026-01-01 21:39:00 +0530

    Add multiple outputs for EKS and VPC resources
    Added outputs for VPC ID, subnet IDs, cluster name, cluster endpoint, cluster certificate authority data, node group name, and node role ARN.

--- output.tf
+++ output.tf
@@ -1,16 +1,34 @@
-output "cluster_id" {
-  value = aws_eks_cluster.devopsshack.id
-}
-
-output "node_group_id" {
-  value = aws_eks_node_group.devopsshack.id
-}
-
 output "vpc_id" {
-  value = aws_vpc.devopsshack_vpc.id
+  description = "The ID of the VPC"
+  value       = aws_vpc.devopsshack_vpc.id
 }
 
 output "subnet_ids" {
-  value = aws_subnet.devopsshack_subnet[*].id
+  description = "The IDs of the subnets created"
+  value       = aws_subnet.devopsshack_subnet[*].id
 }
 
+output "cluster_name" {
+  description = "The name of the EKS cluster"
+  value       = aws_eks_cluster.devopsshack.name
+}
+
+output "cluster_endpoint" {
+  description = "The endpoint of the EKS cluster"
+  value       = aws_eks_cluster.devopsshack.endpoint
+}
+
+output "cluster_certificate_authority_data" {
+  description = "The certificate authority data for the EKS cluster"
+  value       = aws_eks_cluster.devopsshack.certificate_authority[0].data
+}
+
+output "node_group_name" {
+  description = "The name of the EKS node group"
+  value       = aws_eks_node_group.devopsshack.node_group_name
+}
+
+output "node_role_arn" {
+  description = "The ARN of the IAM role for the node group"
+  value       = aws_iam_role.devopsshack_node_group_role.arn
+}
